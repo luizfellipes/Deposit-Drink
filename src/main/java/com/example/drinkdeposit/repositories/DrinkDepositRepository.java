8d8765bdb3fc64c35d74c3cb256e00a0be4016d6
luizfellipes <luiz.13.silva@gmail.com>
2024-04-30 08:44:23 -0400

    Criado repository

---
+++ src/main/java/com/example/drinkdeposit/repositories/DrinkDepositRepository.java
@@ -0,0 +1,9 @@
+package com.example.drinkdeposit.repositories;
+
+import com.example.drinkdeposit.model.entities.DrinkDeposit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DrinkDepositRepository extends JpaRepository<DrinkDeposit, Integer> {
+}
