0c23d1c189b3aa5a3bd47e8abfaf6dac131d81bd
luizfellipes <luiz.13.silva@gmail.com>
2024-04-30 08:43:45 -0400

    Criado a estrutura basica para iniciar o projeto DrinkDeposit

---
+++ src/main/java/com/example/drinkdeposit/DrinkDepositApplication.java
@@ -0,0 +1,13 @@
+package com.example.drinkdeposit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkDepositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkDepositApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/drinkdeposit/DrinkDepositApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drinkdeposit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkDepositApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
