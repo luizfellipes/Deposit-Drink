5b10624740ae74dc86a7b393e6aa141c4c035f54
luizfellipes <luiz.13.silva@gmail.com>
2024-05-08 11:07:59 -0400

    Criado Swagger

---
+++ src/main/java/com/example/drinkdeposit/swagger/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.drinkdeposit.swagger;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI drinkDepositOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Drink Deposit")
+                        .description("Software of Drink Deposit Control")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+
+}
