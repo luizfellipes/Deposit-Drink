cd1c357b6d7f927fd7c9600b71d2920ea71c348e
luizfellipes <luiz.13.silva@gmail.com>
2024-06-20 10:36:55 -0400

    Configurado validaçao no DTO

--- src/main/java/com/example/drinkdeposit/model/dto/DrinkDTO.java
+++ src/main/java/com/example/drinkdeposit/model/dto/DrinkDTO.java
@@ -1,6 +1,14 @@
 package com.example.drinkdeposit.model.dto;
 
 import com.example.drinkdeposit.model.enums.DrinkType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record DrinkDTO(DrinkType drinkType, String drinkName, int volume) {
+public record DrinkDTO(
+        @NotNull(message = "Insira o tipo de bebida !")
+        DrinkType drinkType,
+        @NotBlank(message = "Insira o nome da bebida !")
+        String drinkName,
+        @NotNull(message = "Insira o volume desejado !")
+        int volume) {
 }
--- src/main/java/com/example/drinkdeposit/model/dto/DrinkDepositDTO.java
+++ src/main/java/com/example/drinkdeposit/model/dto/DrinkDepositDTO.java
@@ -2,10 +2,24 @@ package com.example.drinkdeposit.model.dto;
 
 import com.example.drinkdeposit.model.entities.Drink;
 import com.example.drinkdeposit.model.enums.MovimentType;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-
-public record DrinkDepositDTO(LocalDateTime data, MovimentType movimentType, String responsible, String[] section,
-                              Drink drink) {
+public record DrinkDepositDTO(
+        @NotNull(message = "Insira uma data valida !")
+        LocalDateTime data,
+        @NotNull(message = "Insira um tipo de movimentação a ser feita.")
+        MovimentType movimentType,
+        @NotBlank(message = "Insira o responsavel da movimentação.")
+        String responsible,
+        @NotNull(message = "Insira uma seção valida !")
+        List<String> section,
+        @Valid
+        @NotNull
+        DrinkDTO drink) {
 }
