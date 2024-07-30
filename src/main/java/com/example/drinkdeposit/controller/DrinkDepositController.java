6a6964a105975da56190630f0bb395737b25bd05
luizfellipes <luiz.13.silva@gmail.com>
2024-07-30 11:09:44 -0400

    Adicionado safe cors segundo sonarqube

--- src/main/java/com/example/drinkdeposit/controller/DrinkDepositController.java
+++ src/main/java/com/example/drinkdeposit/controller/DrinkDepositController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RequestMapping("/drinkdeposit")
 public class DrinkDepositController {
 
